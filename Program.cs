23b2464e8dbc931db4f768e3eaee5b01d86dbd40
fishman123456 <den9152@yandex.ru>
2023-11-28 00:54:18 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace HttpExampl_28_11_2023
+{
+    internal class Program
+    {
+        static  async Task Main(string[] args)
+        {
+            // установить программу postman ---- 28-11-2023
+            // // 1.создание http - клиента
+            //HttpClient httpClient = new HttpClient();
+            // // 2.выполнение запроса
+            // HttpResponseMessage respons = await httpClient.GetAsync("http://numbersapi.com/random");
+            // // 3. считать
+            // string responsBody =  await respons.Content.ReadAsStringAsync();
+            // Console.WriteLine(responsBody);
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage respons = await httpClient.GetAsync($"http://numbersapi.com/{10}");
+            string responsBody =  await respons.Content.ReadAsStringAsync();
+            Console.WriteLine(responsBody);
+            Console.WriteLine($"Код ответа: {(int)respons.StatusCode}");
+           
+        }
+    }
+}
